dd6b61b4806dbaf54e8381a435f3df525dfaa203
Dan Jelf <dankjelf@gmail.com>
2024-11-22 21:16:22 -0500

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "clout",
   css: {
     devSourcemap: true,
   },
   plugins: [vue()],
 });
+
